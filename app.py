e0e5717398451d98c30f369430c68304a8073c17
e18fourn <elsa.fourneau@imt-atlantique.net>
2020-02-26 17:49:27 +0100

    bjr
    correction bugs

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import flask
 from flask import Flask
 
-from database.database import db, init_database
+#from database.database import db, init_database
 
 app: Flask = Flask(__name__)
 
-db.init_app(app)                        # (1) flask prend en compte la base de donnee
-with app.test_request_context():         # (2) bloc execute a l'initialisation de Flask
+#db.init_app(app)                        # (1) flask prend en compte la base de donnee
+#with app.test_request_context():         # (2) bloc execute a l'initialisation de Flask
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/database.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/database.db"
+#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route('/Accueil')
+@app.route('/accueil')
 def afficher_accueil():
    return flask.render_template("Accueil.html.jinja2")
 
@@ -20,9 +20,7 @@ def afficher_accueil():
 def master():
     return flask.render_template("Master.html.jinja2")
 
-@app.route('/Reservation')
-def reservation():
-    return flask.render_template("Reservation.html.jinja2")
+
 
 
 
