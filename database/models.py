57bfc00ee32b17be8819bf60e4b2faa0ce1588df
apogrll <apolline.rg@gmail.com>
2020-03-04 14:51:53 +0100

    f

--- database/models.py
+++ database/models.py
@@ -1,6 +1,5 @@
 from database.database import db
 
-
 class Sandwichs (db.Model):
     id_s = db.Column(db.Integer, primary_key=True)
     nom_s = db.Column(db.Text)
@@ -10,14 +9,15 @@ class Sandwichs (db.Model):
     quantite_restante = db.Column(db.Integer)
     est_epuise = db.Column(db.Boolean)
 
-class Reservation (db.Model):
-    id_r = db.Column(db.Integer, primary_key=True)
-    nom_client = db.Column(db.Text)
-    prenom_client = db.Column(db.Text)
-    nom_s = db.Column(db.Text)
-    quantite_reservation = db.Column(db.Integer)
-
 class Client (db.Model):
     id_c = db.Column(db.Integer, primary_key=True)
     nom_client = db.Column(db.Text)
     prenom_client = db.Column(db.Text)
+
+class Reservation (db.Model):
+    id_r = db.Column(db.Integer, primary_key=True)
+    db.column('nom_client', db.Text, db.ForeignKey('Client.nom_client')),
+    db.column('prenom_client', db.Text, db.ForeignKey('Client.prenom_client')),
+    db.column('nom_s', db.Text, db.ForeignKey('Sandwichs.nom_s')),
+    quantite_reservation = db.Column(db.Integer)
+
