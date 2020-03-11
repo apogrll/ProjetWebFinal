7502e461ae19cf49abc18104a41ad8c74933f095
e18fourn <elsa.fourneau@imt-atlantique.net>
2020-03-11 10:53:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -104,13 +104,11 @@ def do_reservation():
 @app.route('/<cat>')
 def produits(cat):
 
-
-
     #quantite_restante = Produits.query.filter_by(categorie=cat).count()
     list_produits=Produits.query.filter_by(categorie=cat).all()
     client = Client.query.filter_by(id_c=1).first()
 
-    return flask.render_template("PageCardProduits.html.jinja2", sandwich_1=list_produits, categorie = cat, client=client)
+    return flask.render_template("PageCardProduits.html.jinja2", sandwichs_1=list_produits, categorie = cat, client=client)
 
 #def quantite(prod):
 #    quantite_dict = {}
